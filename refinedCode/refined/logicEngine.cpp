3bd2a051cfd9ab9d238713cb70f5a52a335b5131
Marcu Tudor <MRTSoft@users.noreply.github.com>
2014-07-24 17:43:47 +0300

    Created new folders.
    Began working on math logistic

--- refinedCode/refined/logicEngine.cpp
+++ refinedCode/refined/logicEngine.cpp
@@ -5,13 +5,53 @@
  * http://creativecommons.org/licenses/by-nc-sa/4.0/
  * or read the license offline in the License folder
  */
- 
+
  /*
  * LOGIC ENGINE
  * Contains the functions needed for game logistic:
  *     -> Generating the maze
  *     -> Reward system and others
- *
-*/
+ */
+
+//INCLUDE LIBRARIES
+// #include <iostream> // do we need this?
+#include <stdlib.h>
+#include <time.h>
+// using namespace std; //do we need this?
+
+//variables, constants and data types
+    //define directions as integers
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
+    //define constrains
+#define MAX_H 50  //depends....
+#define MAX_W 100 //depends....
+    //define data types (cell)
+struct Cell
+{
+    bool u, d, l, r;//the walls of the cell (up, down, left, right)
+    int val; //the value of the cell
+};
+
+// in the C array we have all the possible arrangements  of 4
+//const unsigned C[24] = {1234, 1243, 1324, 1342, 1423, 1432, 2134, 2143, 2314, 2341, 2413, 2431, 3124, 3142, 3214, 3241, 3412, 3421, 4123, 4132, 4213, 4231, 4312, 4321}; //initial ordered array
+  const unsigned C[24] = {1423, 1324, 3214, 4231, 4132, 2341, 4123, 3142, 1342, 3421, 1432, 4213, 2143, 4321, 2431, 3124, 2314, 4312, 3241, 1234, 2413, 3412, 1243, 2134}; //final randomized array
+int mW, mH;// globally stores the maze Width and height
+Cell * generateMaze(int width, int height)//the main function of the file
+{
+    //validate the parameters
+    if (width < 1 || width > MAX_W)
+    {
+        std::cerr<<"EROR: Width out of bounds!";
+        return NULL;
+    }
+    if (height < 1 || height > MAX_H)
+    {
+        std::cerr<<"EROR: Height out of bounds!";
+        return NULL;
+    }
+}
 
 
