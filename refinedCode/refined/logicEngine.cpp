de12f1bd6ea34ed46f7d300d4e788fae875bfa46
Marcu Tudor <MRTSoft@users.noreply.github.com>
2014-07-24 16:59:37 +0300

    Added main files (refined code) and license folder
    The refine code folder is 'where the magic happens'. The license
    provides support for... license

---
+++ refinedCode/refined/logicEngine.cpp
@@ -0,0 +1,18 @@
+/* COPYRIGHT (C) Marcu Tudor 2014 MR
+ * This work is licensed under the
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc-sa/4.0/
+ * or read the license offline in the License folder
+ */
+ 
+ /*
+ * LOGIC ENGINE
+ * Contains the functions needed for game logistic:
+ *     -> Generating the maze
+ *     -> Reward system and others
+ *
+
+*/
+
+
